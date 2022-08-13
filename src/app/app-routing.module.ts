0544c59c290cc46308fc340548874f33eb12e66f
anouar019 <anouar019@gmail.com>
2022-08-13 10:28:05 +0200

    20220813

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientListComponent } from './components/patient/patient-list/patient-list.component';
+import { PatientUpdateComponent } from './components/patient/patient-update/patient-update.component';
+import { PatientComponent } from './components/patient/patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"patient", component: PatientComponent},
+  {path:"patientList", component: PatientListComponent},
+  {path:"patientUpdate", component: PatientUpdateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientComponent } from './components/patient/patient.component';
+import { PatientListComponent } from './components/patient/patient-list/patient-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PatientCreateComponent } from './components/patient/patient-create/patient-create.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GlobalConstComponent } from './const/global-const/global-const.component';
+import { PatientUpdateComponent } from './components/patient/patient-update/patient-update.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientComponent,
+    PatientListComponent,
+    PatientCreateComponent,
+    GlobalConstComponent,
+    PatientUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FormsModule,
+
+    CommonModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule ,
+
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
